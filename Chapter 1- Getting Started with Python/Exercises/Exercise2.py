4106b4644eabdf6d43ccfebba99d93eb7e6e5ae8
jojoannaJC <joannachristabel7@gmail.com>
2023-09-28 13:58:44 +0400

    Exercise2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise2.py
@@ -0,0 +1,3 @@
+import datetime
+d= datetime.datetime(2023, 10, 28)
+print (d)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+from datetime import date, time, datetime
+#d= datetime.datetime(2023, 9, 28, 1, 50, 23)
+#print(d)
+#f=date.today()
+#print(f)
+now=datetime.now()
+print(now)
