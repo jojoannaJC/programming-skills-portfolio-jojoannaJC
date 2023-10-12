5de3057323150f3ebd4aa779c93f626bbcf0836a
jojoannaJC <joannachristabel7@gmail.com>
2023-10-12 13:49:28 +0400

    File1.py

--- Chapter 1- Getting Started with Python/Exercises/File1.py
+++ Chapter 1- Getting Started with Python/Exercises/File1.py
@@ -1,4 +1,5 @@
 #Q1: Write a program to output the letters W, X, Y, Z, each on a separate line.  
-print("x")
-print("y")
-print("z")
+print("""w, \n
+x, \n
+y, \n
+z""")
---
+++ Chapter 1- Getting Started with Python/Exercises/File5.py
@@ -0,0 +1,6 @@
+#Q5: Write four strings in different variables and print the output as one string. 
+name="Joanna"
+age=17
+course="Creative Computing"
+module_name="CODELAB 1"
+print(f"{name},{age},{course},{module_name}")
