f6406c6399bde89015e3d1bc6742202dca71c528
jojoannaJC <joannachristabel7@gmail.com>
2023-10-12 13:54:36 +0400

    File3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/File3.py
@@ -0,0 +1,10 @@
+#Q3: You need to make a program for a leader board. The program needs to output the numbers 1-9, each ona separate line followed by a dot.
+print("""1.
+2.
+3.
+4. 
+5.
+6. 
+7. 
+8. 
+9. """)
