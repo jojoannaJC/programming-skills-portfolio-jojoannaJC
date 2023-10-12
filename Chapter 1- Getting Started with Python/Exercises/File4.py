bcd4dd41db5124069a4f862ef90ce1dca0eacef3
jojoannaJC <joannachristabel7@gmail.com>
2023-10-12 13:56:00 +0400

    File4.py

--- Chapter 1- Getting Started with Python/Exercises/File4.py
+++ Chapter 1- Getting Started with Python/Exercises/File4.py
@@ -1,5 +1,5 @@
 #Q4: Compute the area of a triangle.
-b=int(input("Base of Triangle="))
-h=int(input("Height of Triangle="))
+b=int(input("Base of Triangle= "))
+h=int(input("Height of Triangle= "))
 area=b*h/2
 print("Hence, Area of Triangle is", area)
