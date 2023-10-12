184b9f55d46a0bfc76120661b07338de92e100e1
jojoannaJC <joannachristabel7@gmail.com>
2023-10-12 13:18:48 +0400

    Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_files.py/end.py
@@ -0,0 +1,19 @@
+#first name, last name : print in one string
+fname="Joanna "
+lname="Christabel"
+name=fname + lname
+print(name)
+
+print(f'Hello {fname}.')
+print(name, end="\t")
+print(" new line message")
+
+print("Its me \n hi \n Im the problem \n Its me :)") 
+
+print(f"Merry Christmas")
+
+print(f"The value is {10 + 5}.")
+
+val=20
+print(f"The value is {val / 2}.")
+
---
+++ Chapter 1- Getting Started with Python/Exercises/File1.py
@@ -0,0 +1,4 @@
+#Q1: Write a program to output the letters W, X, Y, Z, each on a separate line.  
+print("x")
+print("y")
+print("z")
