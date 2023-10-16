db37abf30bdfd148bb6bc4176d33ffa88fa6b257
jojoannaJC <joannachristabel7@gmail.com>
2023-10-16 10:03:03 +0400

    Exercise2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -1,2 +1,2 @@
 #Find a quote from a famous person you admire. Print the quote and the name of its author. 
-print("Joanna Christabel once said- SLAY.")
+print("William Shakespeare once said- 'To be or Not to be. That is the question.' ")
