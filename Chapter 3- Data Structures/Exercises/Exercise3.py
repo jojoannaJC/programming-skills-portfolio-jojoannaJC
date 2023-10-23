5beba7df7ee75bb859c626e3aea9edd0be690df4
jojoannaJC <joannachristabel7@gmail.com>
2023-10-23 10:01:29 +0400

    Exercise3.py

--- Chapter 3- Data Structures/Exercises/Exercise3.py
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -1,11 +1,13 @@
 #Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 #Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
-v=["Car", "Bus", "Bike", "Bicycle", "Airplane"]
+v=["Car", "Bus", "Bike", "Bicycle", "Airplane", "Private Jet"]
 
 print("I would like to own a " + v[0].title())
 print("I would like to own a " + v[1].title())
 print("I would like to own a " + v[2].title())
 print("I would like to own a " + v[3].title())
 print("I would like to own a " + v[4].title())
+print("I would like to own a " + v[5].title())
+
 
