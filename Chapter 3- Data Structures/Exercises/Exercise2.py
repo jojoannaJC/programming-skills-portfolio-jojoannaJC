efa434e05e8eb25d90fdca490c77745fc5cf2d78
jojoannaJC <joannachristabel7@gmail.com>
2023-10-23 09:34:47 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+#Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. 
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+
+names=["Joanna", "Micah", "Aysa", "Gian", "Helen", "Angela", "Aryan"]
+
+print("Hello " + names[0].title())
+print("Hello " +names[1].title())
+print("Hello " +names[2].title())
+print("Hello " +names[3].title())
+print("Hello " +names[4].title())
+print("Hello " +names[5].title())
+print("Hello " +names[6].title())
+
---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,11 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+v=["Car", "Bus", "Bike", "Bicycle", "Airplane"]
+
+print("I would like to own a " + v[0].title())
+print("I would like to own a " + v[1].title())
+print("I would like to own a " + v[2].title())
+print("I would like to own a " + v[3].title())
+print("I would like to own a " + v[4].title())
+
